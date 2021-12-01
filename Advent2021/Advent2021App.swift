76d2c076802f835c816577a21244f4c9d1cae2a4
Matthew Thompson <thompson.matthew@heb.com>
2021-12-01 13:49:22 -0600

    Initial Commit

---
+++ Advent2021/Advent2021App.swift
@@ -0,0 +1,17 @@
+//
+//  Advent2021App.swift
+//  Advent2021
+//
+//  Created by Thompson,Matthew on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Advent2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
